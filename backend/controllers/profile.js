c5bbad2a8b45997a727a5f577e2527270052e090
vishalspatil <vishalspolicepatil55@gmail.com>
2023-08-21 01:21:49 +0530

    updated

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const FormData = require('form-data');
 const { extract } = require("./Extract");
 const { get_structured_data } = require("./get_structured_data")
-const {User} = require('../Database/Models')
+const { User } = require('../Database/Models')
 
 data = {
     users: [{
@@ -23,38 +23,40 @@ var storage = multer.diskStorage({
     }
 })
 
-const send_xml = async (filename) => {
-    try {
-        const file = fs.createReadStream(`uploads/${filename}`);
-        const title = 'digital kyc xml file';
+const send_xml_promise = function (filename) {
+    return new Promise(function (resolve, reject) {
+        try {
+            const file = fs.createReadStream(`uploads/${filename}`);
+            const title = 'digital kyc xml file';
 
-        const form = new FormData();
-        form.append('title', title);
-        form.append('file', file);
+            const form = new FormData();
+            form.append('title', title);
+            form.append('file', file);
 
-        const resp = await axios.post('http://localhost:5000/verify/upload', form, {
-            headers: {
-                ...form.getHeaders(),
-            }
-        });
-
-        if (resp.status === 200) {
-            return true; //signature is valid
+            axios.post('http://localhost:5000/verify/upload', form, {
+                headers: {
+                    ...form.getHeaders(),
+                }
+            })
+                .then(resp => {
+                    if (resp.data.msg === 'OK') {
+                        resolve("OK");
+                    }
+                    else {
+                        resolve("failed")
+                    }
+                })
+        } catch (err) {
+            return new Error(err.message);
         }
-        else if (resp.status === 406) {
-            return false; //signature is not valid
-        }
-    } catch (err) {
-        return new Error(err.message);
-    }
+    });
 }
-
 
 // function for route handling  
 upload_xml = async (req, res) => {
     var upload = multer({ storage: storage }).single('file');
 
-    upload(req, res, function async(error) {
+    upload(req, res, async function (error) {
 
         if (error) {
             return res.end('Error Uploading File');
@@ -65,30 +67,33 @@ upload_xml = async (req, res) => {
 
             console.log("file uploaded : ", filename);
 
-            send_xml(filename).then(async (resp) => {
-                if (resp) {
-                    extract(filename.slice(0, -4), pass)
-                    .then(() => {
-                        get_structured_data(filename.slice(0, -4))
-                        .then((user_data) => {
-                            user_data.account_address = account_address;
-                            const user = new User(user_data);
-                            user.save();
-                            res.json({user_data});
-                        }).catch(err => {
-                            console.log('xml parsing failed', err);
+            send_xml_promise(filename)
+                .then((x) => {
+                    if (x == "OK") {
+                        console.log('signature verified');
+                        extract(filename.slice(0, -4), pass)
+                            .then(() => {
+                                get_structured_data(filename.slice(0, -4))
+                                    .then((user_data) => {
+                                        user_data.account_address = account_address;
+                                        const user = new User(user_data);
+                                        user.save();
+                                        res.json({ user_data });
+                                    }).catch(err => {
+                                        console.log('xml parsing failed', err);
+                                    })
+                            }).catch(err => {
+                                console.log('extraction failed', err);
+                            })
+                        return;
+                    }
+                    else {
+                        res.json({
+                            "msg": "failed"
                         })
-                    }).catch(err => {
-                        console.log('extraction failed', err);
-                    })
-
-                    console.log('signature is valid');
-                }
-                else {
-                    res.status(406).send('Invalid signature');
-                    console.log('signature is not valid');
-                }
-            });
+                        return;
+                    }
+                })
         }
     });
 }
